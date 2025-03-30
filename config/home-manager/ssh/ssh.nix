9ee1de6e17e2f6bdf8cea35825f363ef93bb1358
Bryan Gass <bryank123@live.com>
2025-03-30 00:50:54 -0400

    fixed ssh.nix to work with default identityAgent

--- config/home-manager/ssh/ssh.nix
+++ config/home-manager/ssh/ssh.nix
@@ -1,16 +1,50 @@
+# {
+#   programs.ssh = {
+#     enable = true;
+#     addKeysToAgent = "yes";
+
+#     # Use correct identification
+#     matchBlocks = {
+#       "github" = {
+#         user = "git";
+#         hostname = "github.com";
+#         identitiesOnly = true;
+#         # Use a reference to the SSH key from your GPG key
+#         # identityFile = "~/.dots/config/home-manager/keychain/id_rsa_yubikey.pub";
+#       };
+#       "mydesktop" = {
+#         user = "beegass";
+#         hostname = "130.44.135.194";
+#         port = 7569;
+#       };
+#     };
+
+#     # Additional SSH options
+#     extraConfig = ''
+#       # Ensure keys are added to the agent
+#       AddKeysToAgent yes
+
+#       IdentityAgent none
+#       IdentityAgent ''${SSH_AUTH_SOCK}
+#     '';
+#   };
+# }
+
+# config/home-manager/ssh/ssh.nix
+# config/home-manager/ssh/ssh.nix
 {
   programs.ssh = {
     enable = true;
-    addKeysToAgent = "yes";
 
-    # Use correct identification
     matchBlocks = {
       "github" = {
         user = "git";
         hostname = "github.com";
         identitiesOnly = true;
-        # Use a reference to the SSH key from your GPG key
-        # identityFile = "~/.dots/config/home-manager/keychain/id_rsa_yubikey.pub";
+        extraOptions = {
+          # Only set the desired IdentityAgent for this host
+          IdentityAgent = "\${SSH_AUTH_SOCK}";
+        };
       };
       "mydesktop" = {
         user = "beegass";
@@ -19,13 +53,17 @@
       };
     };
 
-    # Additional SSH options
     extraConfig = ''
-      # Ensure keys are added to the agent
-      AddKeysToAgent yes
+      Host *
+        # ... other Host * options ...
+        AddKeysToAgent yes
 
-      IdentityAgent none
-      IdentityAgent ''${SSH_AUTH_SOCK}
+        # You might still want 'IdentityAgent none' globally
+        # IF you want to prevent SSH from automatically trying
+        # default agent paths for hosts NOT explicitly configured
+        # with a specific IdentityAgent. If you *only* ever use
+        # the gpg-agent socket, this might be okay.
+        # IdentityAgent none 
     '';
   };
 }
